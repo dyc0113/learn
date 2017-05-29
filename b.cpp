9815739af165d259d9afc3401ad42893c195e943
dyc0113 <dyc0113@163.com>
2017-05-29 00:36:05 -0700

    提交

---
+++ b.cpp
@@ -0,0 +1,9 @@
+/*************************************************************************
+	> File Name: b.cpp
+	> Author: dyc0113
+	> Mail: dyc0113@163.com 
+	> Created Time: Tue 17 Jan 2017 08:24:49 AM PST
+ ************************************************************************/
+#include<iostream>
+#include "b.h"
+using namespace std;
