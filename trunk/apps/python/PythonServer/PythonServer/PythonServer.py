746ae17f8fc60039ac4a89f019af54c090de605d
dyc0113 <dyc0113@163.com>
2017-06-25 17:47:41 +0800

    server

--- trunk/apps/python/PythonServer/PythonServer/PythonServer.py
+++ trunk/apps/python/PythonServer/PythonServer/PythonServer.py
@@ -5,11 +5,18 @@ import json
 #文件传输的例子 
 #http://www.cnblogs.com/dreamer-fish/p/5501924.html
 
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
+                datefmt='%a, %d %b %Y %H:%M:%S',
+                filename='server.log',
+                filemode='w')
+
 def printWindow(contentData):
     print contentData.decode("UTF-8").encode("GBK")
 
 #获取请求的内容json串
-def GetContentData(connection, dataLen):
+def RecevData(connection, dataLen):
      #接收报文内容信息
     try:
         contentData=""
@@ -30,6 +37,49 @@ def GetContentData(connection, dataLen):
         print ex
     return contentData
 
+#接受客户端数据，并根据协议解析出dict结构返回
+def RecevDictData(conn, address):
+    iRet = 0
+    fileinfo_size=struct.calcsize('128sl') #定义文件信息。128s 命令字 ，l表示一个int或log文件类型，在此为文件大小
+    data_buf = conn.recv(fileinfo_size) #接受报文头信息
+    #输出报文头信息
+    cmd,dataLen =struct.unpack('128sl', data_buf)
+    print "cmd:",cmd,"  dataLen:", dataLen;
+    contentData = RecevData(conn, dataLen);
+    printWindow("receive data:"  + contentData) 
+    dReq = json.loads(contentData) #
+    dReq["cmd"] = cmd;
+    return iRet, dReq;
+
+#想对端发送一个命令字
+def SendData(s, cmd, contentData):
+    try:
+
+        fileinfo_size=struct.calcsize('128sl') #定义打包规则
+        #定义文件头信息，包含文件名和文件大小
+        fhead = struct.pack('128sl',cmd , len(contentData))
+        print "send head"
+        s.send(fhead) 
+        print "send data content", contentData.decode("UTF-8").encode("GBK")
+        s.send(contentData)
+        print 'send over...'
+
+    except Exception,ex:
+        print "exceprion", e
+
+#发送字典数据
+def SendDictData(conn, dData):
+    jsonReq = json.dumps(dData, ensure_ascii=False, sort_keys=True)
+    print jsonReq.encode("gbk")
+    SendData(conn, "rsp", jsonReq);
+
+
+def processTestCmd(mReq):
+    iRet = 0;
+    mRsp ={}
+    mRsp["rsp"] = "success" 
+    return iRet, mRsp;
+
 def SocketServer():
     try:
         Colon = ServerUrl.find(':')
@@ -44,26 +94,25 @@ def SocketServer():
         sockobj.listen(5)
  
         while True:
-            connection, address = sockobj.accept( )
-            print 'Server connected by client:', address
+            try:
+                connection, address = sockobj.accept( )
+                print 'Server connected by client:', address
 
-            fileinfo_size=struct.calcsize('128sl') #定义文件信息。128s 命令字 ，l表示一个int或log文件类型，在此为文件大小
-            data_buf = connection.recv(fileinfo_size) #接受报文头信息
-            #输出报文头信息
-            cmd,dataLen =struct.unpack('128sl', data_buf)
-            print "cmd:",cmd,"  dataLen:", dataLen;
-
-            contentData = GetContentData(connection, dataLen);
-
-            printWindow("receive data:"  + contentData) 
-
-            #data = connection.recv(1024)
-            #if not data: break
-            #RES='200 OK'
-            #connection.send(RES)
-            #print 'Receive MSG:%s'%data.strip()
-            #print 'Send RES:%s\r\n'%RES
-            connection.close( )
+                iRet, dReq = RecevDictData(connection, address); #收包
+                if (iRet != 0):
+                    logging.error('RecevDictData faild. iRet:%d', iRet)
+                    break
+                mRsp = {} 
+                logging.debug('dReq:%s', str(dReq));
+                if "test" in dReq["cmd"]:
+                    iRet,mRsp = processTestCmd(dReq);
+          
+                #回包
+                logging.debug('begin SendDictData:%s', str(mRsp));
+                SendDictData(connection, mRsp);
+                connection.close( )
+            except Exception,ex:
+                print ex
 
     except Exception,ex:
         print ex
