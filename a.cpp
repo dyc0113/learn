fa4c4cefd603a5587cd944a1230628dc04736d02
董冲 <dongchong@MacBook-Pro.local>
2020-09-28 18:08:44 +0800

    add

--- a.cpp
+++ a.cpp
@@ -1,6 +1,32 @@
-#include "a.h"
+// thread example  
+#include <iostream>       // std::cout  
+#include <thread>         // std::thread  
 
-void print(string str)
-{
-	cout<<str.c_str()<<endl;
-}
+void thr_function1()  
+{  
+    for (int i = 0; i != 10; ++i)  
+    {  
+        std::cout << "thread 1 print " << i << std::endl;  
+    }  
+}  
+
+void thr_function2(int n)  
+{  
+    std::cout << "thread 1 print " << n << std::endl;  
+}  
+
+int main()  
+{  
+    std::thread t1(thr_function1);     // spawn new thread that calls foo()  
+    std::thread t2(thr_function2, 111);  // spawn new thread that calls bar(0)  
+
+    std::cout << "main, foo and bar now execute concurrently...\n";  
+
+    // synchronize threads:  
+    t1.join();                // pauses until first finishes  
+    t2.join();               // pauses until second finishes  
+
+    std::cout << "thread 1 and htread 2 completed.\n";  
+
+    return 0;  
+}  
